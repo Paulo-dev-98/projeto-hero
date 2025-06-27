ec7d007a0d537af83cf5baf262d2ef7dc9857193
Paulo-dev-98 <paulobg8@gmail.com>
2025-06-27 13:12:04 -0300

    ajustando o cors

--- Projeto da cadastro de heroi/src/main/java/br/com/erudio/controller/HeroiController.java
+++ Projeto da cadastro de heroi/src/main/java/br/com/erudio/controller/HeroiController.java
@@ -13,6 +13,7 @@ import org.springframework.data.web.PagedResourcesAssembler;
 import org.springframework.hateoas.PagedResources;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,7 +29,7 @@ import br.com.erudio.services.HeroiServices;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 
-//@CrossOrigin
+@CrossOrigin(origins = "http://localhost:4200")
 @Api(tags = "EndpointHeroi")
 @RestController
 @RequestMapping("/api/heroi/v1")
